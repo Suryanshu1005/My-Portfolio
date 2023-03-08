5c8aaa5532e53ae4d8407c449fcb323daa7b9918
Suryanshu <tshuryanshu@gmail.com>
2023-03-08 14:47:34 +0530

    changes in computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -20,7 +20,7 @@ const Computers = ({ isMobile }) => {
       <primitive 
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3,-2.2] : [0, -3.25, -1.5]}
+        position={isMobile ? [0, -3,-2.2] : [0, -3.0, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -49,10 +49,11 @@ const ComputersCanvas = () => {
   return (
     <Canvas frameloop="demand"
       shadows dpr={[1,2]}
-      camera={{position: [20,3,5], fov:25}}
+      camera={{position: [20,3,10], fov:25}}
       gl={{ preserveDrawingBuffer: true}}>
         <Suspense fallback={<CanvasLoader />}>
           <OrbitControls 
+          autoRotate
           enableZoom={false} 
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
