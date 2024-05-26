634f1395779078036a3f5793b05fa1262f651657
Suryanshu Tiwari <59340699+Suryanshu1005@users.noreply.github.com>
2024-05-26 11:02:01 +0530

    Update Resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ import { SectionWrapper } from '../hoc';
 import { styles } from '../style';
 import { motion } from 'framer-motion';
 import { textVariant } from '../utils/motion';
-import resume from '../assets/Suryanshu_Tiwari_updated.pdf';
+import resume from '../assets/Suryanshu_Tiwari_FE.pdf';
 
 const Resume = () => {
   return (
@@ -13,7 +13,7 @@ const Resume = () => {
         <h2 className={`${styles.sectionHeadText}`}>Resume</h2>
         <p className={`${styles.sectionSubText}`}>Click below to download my resume.</p>
       </motion.div>
-      <a href={resume} download="Suryanshu_Tiwari_resume.pdf">
+      <a href={resume} download="Suryanshu_Tiwari_7860467604.pdf">
       <button type='button'
             className='py-4 px-6 bg-tertiary mt-4 justify-center items-center
                 text-white rounded-xl c outline-none w-fit shadow-md shadow-primary mx-auto' 
